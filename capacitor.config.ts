d5bf3d06153aa06aad60e551351bd477d20bbb6e
Carter <Carts2003@gmail.com>
2024-03-20 00:07:20 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AlienLifeFrequency',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
