d1ee36254131ee7c0b9f9712a9fa6c0c0c87ce17
CarterMcClure1 <155032293+CarterMcClure1@users.noreply.github.com>
2024-03-23 04:33:24 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonPage, IonTitle, IonToolbar, IonButton, useIonRouter } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { useHistory } from 'react-router-dom';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const navigation = useIonRouter();
+  const history = useHistory(); // Initialize useHistory
+
+  // Function to handle button click and navigate to Intro page
+  const goToIntroPage = () => {
+    navigation.push('/intro', 'forward', 'replace')
+  };
+  
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+      <IonToolbar className="toolbar">
+      <div className="logo-title-container">
+    <img src='./images/alienlifefreqapp.png' alt="image" className="logo" />
+    <IonTitle className="title">Alien Life Frequency</IonTitle>
+  </div>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +29,12 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <div className="centered-content">
+          
+          <IonItem> This will be the home page</IonItem>
+        </div>
+        <IonButton expand="full" onClick={() => goToIntroPage()}>Go to Intro</IonButton>
+        
       </IonContent>
     </IonPage>
   );
