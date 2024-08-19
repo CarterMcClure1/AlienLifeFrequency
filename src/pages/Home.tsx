c803b0415fa69d6098764938e928dd05e84778a7
CarterMcClure1 <155032293+CarterMcClure1@users.noreply.github.com>
2024-08-19 17:32:10 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,31 @@
-import { IonContent, IonHeader, IonItem, IonIcon, IonPage, IonTitle, IonToolbar, IonToggle, IonButton, useIonRouter, IonMenu, IonMenuButton, IonSplitPane, IonMenuToggle, IonRouterOutlet, IonCard, IonCardTitle, IonCardContent, IonCardHeader } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import { Route, useHistory } from 'react-router-dom';
-import { homeOutline, newspaperOutline, logOutOutline, menuOutline, megaphoneOutline} from 'ionicons/icons'
-import './Home.css';
-import Menu from "./Menu"
-import Intro from './Intro';
+import React from 'react';
+import { IonContent, IonHeader, IonItem, IonIcon, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonCardTitle, IonCardContent, IonCardHeader } from '@ionic/react';
+import { megaphoneOutline, logoFacebook, logoInstagram, logoYoutube, cartOutline, musicalNotesOutline } from 'ionicons/icons';
+import './Home.css'; 
+import newGalaxyBackground from '../assets/newgalaxy.jpg';  // Updated path
 
 const Home: React.FC = () => {
 
-  const paths = [
-    { name: 'Home', url: 'app/home', icon: homeOutline },
-    { name: 'Intro', url: 'app/intro', icon: newspaperOutline }
-]
-
-const announcements = [
-  { id: 1, title: 'New Album Release!', content: 'Our new album "Dreamscape" is out now. Listen on all major platforms.' },
-  { id: 2, title: 'Upcoming Tour', content: 'Exciting news! We\'re going on tour next month. Stay tuned for dates and locations.' },
-  { id: 3, title: 'Exclusive Merch', content: 'Check out our exclusive merchandise collection available now on our website.' },
-];
-
-  const navigation = useIonRouter();
-  const history = useHistory(); // Initialize useHistory
-
-  // Function to handle button click and navigate to Intro page
-  const goToIntroPage = () => {
-    navigation.push('/intro', 'forward', 'replace')
-  };
-  
+  const announcements = [
+    { id: 1, title: 'New Album Release!', content: 'Our new album "Dreamscape" is out now. Listen on all major platforms.' },
+    { id: 2, title: 'Upcoming Tour', content: 'Exciting news! We\'re going on tour next month. Stay tuned for dates and locations.' },
+    { id: 3, title: 'New Merch', content: 'Check out our exclusive merchandise collection available now on our website (Link Below).' },
+  ];
 
   return (
-    
     <IonPage>
-      
-       {/* Include the Menu component here? */}
       <IonHeader>
-      <IonToolbar className="toolbar">
-      <div className="logo-title-container">
-    <IonTitle className="title">Alien Life Frequency
-    </IonTitle>
-    <img src='./images/alienlifefreqapp.png' alt="image" className="logo" />
-    
-   {/*<IonToggle class="toggle">Light Mode</IonToggle>*/}
-
-  </div>
+        <IonToolbar className="toolbar">
+          <div className="logo-title-container">
+            <IonTitle className="title">Alien Life Frequency</IonTitle>
+            <img src='./images/alienlifefreqapp.png' alt="image" className="logo" />
+          </div>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen style={{ 
+        background: `url(${newGalaxyBackground}) no-repeat center center fixed`, 
+        backgroundSize: 'cover'
+      }}>
         <div className="announcements-box">
           <IonIcon icon={megaphoneOutline} className="announcement-icon" />
           <h2 className="box-title">Announcements</h2>
@@ -55,38 +35,46 @@ const announcements = [
               <p>{announcement.content}</p>
             </IonItem>
           ))}
-          
         </div>
-      
-        <IonCard className="customOrbitronFont" color = "dark">
-  <IonCardHeader>
-    <IonCardTitle>Check Out Our Most Popular Song!</IonCardTitle>
-  </IonCardHeader>
-  <IonCardContent>
-    <audio src="./assets/audio/test.mp3" controls></audio>
-  </IonCardContent>
-</IonCard>
-      
-<IonCard>
-        <IonHeader>
-          <IonCardTitle>Watch Our Most Recent Video!</IonCardTitle>
-        </IonHeader>
-        <IonCardContent>
-        <iframe width="100%" height="315" src="https://www.youtube.com/embed/7WWxB4xNeO8?si=G-iJ2tWyS-R6Gw11"
-       title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-       referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
-        </IonCardContent>
-      </IonCard> 
-        
+        <IonCard className = "homeicons">
+          <IonCardHeader>
+            <IonCardTitle className="box-title2">Check Out Our Merch Store!</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonButton href="https://iagomediaproduction.threadless.com/" target="_blank" className="social-icon" fill="clear">
+              <IonIcon icon={cartOutline} style={{ fontSize: '80px', color: 'black' }} />
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard className = "homeicons">
+          <IonCardHeader>
+            <IonCardTitle className="box-title2">Support us on Other Platforms!</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent className="social-icons-container">
+            <IonButton href="https://www.facebook.com/AlienLifeFrequency/" target="_blank" className="social-icon" fill="clear">
+              <IonIcon icon={logoFacebook} style={{ fontSize: '50px', color: 'blue' }} />
+            </IonButton>
+            <IonButton href="https://www.instagram.com/alienlifefrequency/" target="_blank" className="social-icon" fill="clear">
+              <IonIcon icon={logoInstagram} style={{ fontSize: '50px', color: '#C13584' }} />
+            </IonButton>
+            <IonButton href="https://www.youtube.com/@AlienLifeFrequency" target="_blank" className="social-icon" fill="clear">
+              <IonIcon icon={logoYoutube} style={{ fontSize: '50px', color: '#FF0000' }} />
+            </IonButton>
+            <IonButton href="https://alienlifefrequency.bandcamp.com/album/alien-life-frequency-2" target="_blank" className="social-icon" fill="clear">
+              <IonIcon icon={musicalNotesOutline} style={{ fontSize: '50px', color: 'black' }} />
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
+
       </IonContent>
-      
-      
-      
     </IonPage>
-    
   );
 };
 
 export default Home;
+
+
+
 
